59c327f0d2fd2a120c3601aceb14c6d63985c1b7
Kuibao Zhang <kuibao.zhang@amlogic.com>
2017-11-06 16:29:42 +0800

    BT: enable rc connection parameter updates
    PD#153888
    
    1.set the parameter true
    
    Change-Id: I91d8d9934730542a45653e2549e8c3a6c5fef5a6

--- stack/smp/smp_act.cc
+++ stack/smp/smp_act.cc
@@ -1409,7 +1409,7 @@ void smp_fast_conn_param(tSMP_CB* p_cb, tSMP_INT_DATA* p_data) {
      some peripherals are not able to revert to fast connection parameters
      during the start of service discovery. Connection paramter updates
      get enabled again once service discovery completes. */
-  L2CA_EnableUpdateBleConnParams(p_cb->pairing_bda, false);
+  L2CA_EnableUpdateBleConnParams(p_cb->pairing_bda, true);
 }
 
 /*******************************************************************************
